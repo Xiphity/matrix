f0cb754b269af936094ce096fe1428140f97e21b
Xiphity <h510051@nehs.tc.edu.tw>
2018-08-19 11:24:10 +0800

    ver 1.0

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,12 @@
 using namespace std;
 int main() {
 	int ans[3][3] = { 0, 8, 8, 4, 5, 6, 7, 1, 3 };
+	int i[3][3] = { 1, 0, 0, 0, 2, 0, 0, 0, 1 };
 	Matrix::matrix<int,3> test { ans };
-	std::cout << test << std::endl;
-	double d = test.det();
-	std::cout << d;
+	Matrix::matrix<int,3> I { i };
+	std::cout << test << std::endl << I << std::endl;
+	test = I * test;
+	std::cout << test;
 }
 
 
